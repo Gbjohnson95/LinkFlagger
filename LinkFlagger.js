db4d5837705c48a513fd82c18395f2ae48c5a772
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2015-09-28 08:27:52 -0600

    Added new features.
    It flags links that dont open in new pages, it also flags images that dont have an alt tag.

--- LinkFlagger.js
+++ LinkFlagger.js
@@ -1,22 +1,19 @@
 // ==UserScript==
 // @name        LinkFlagger
+// @version     4
+// @author      Grant Johnson
 // @description Highlights brainhoney and box links and images.
-// @include     *
+// @include     *brightspace.com*
 // @exclude     *brainhoney.com*
 // ==/UserScript==
 
-
 var links = document.getElementsByTagName('a');
-
 var element;
 
 // Flags brainhoney links red
 for (var i = 0; i < links.length; i++) {
-
     element = links[i];
-
-    if (element.href.indexOf("https://byui.brainhoney") == 0) {
-
+    if (element.href.indexOf("https://byui.brainhoney") === 0) {
         element.style.color = "#ff0000";
         element.style.backgroundColor = "#000000";
     }
@@ -24,30 +21,36 @@ for (var i = 0; i < links.length; i++) {
 
 // Flags box links red
 for (var i = 0; i < links.length; i++) {
-
     element = links[i];
-
-    if (element.href.indexOf("https://app.box") == 0) {
-
+    if (element.href.indexOf("https://app.box") === 0) {
         element.style.color = "#ff0000";
         element.style.backgroundColor = "#000000";
     }
 }
 
-// Flags links that dont open in new windows yellow
-//for ( var i = 0; i < links.length; i++ ) {
+// Flags links that don't open in new window
+for (var i = 0; i < links.length; i++) {
+    element = links[i];
+    if (element.target.indexOf("_blank") !== 0 && !element.hasAttribute("class") && element.href.indexOf("http") == 0) {
+        element.style.border = "3px dotted #ffaa00";
+    }
+}
 
-
-// Flags images from brainhoney with red border
 var linksimg = document.getElementsByTagName('img');
 var element;
 
+// Flags images from brainhoney with red border
 for (var i = 0; i < linksimg.length; i++) {
-
     element = linksimg[i];
+    if (element.src.indexOf("https://byui.brainhoney") === 0) {
+        element.style.border = "5px solid #ff0000";
+    }
+}
 
-    if (element.src.indexOf("https://byui.brainhoney") == 0) {
-
-        element.style.border = "10px solid #ff0000";
+// Flags images without alt with a blue border. 
+for (var i = 0; i < linksimg.length; i++) {
+    element = linksimg[i];
+    if (!element.hasAttribute("alt")) {
+        element.style.outline = "5px solid #0000ff";
     }
 }
