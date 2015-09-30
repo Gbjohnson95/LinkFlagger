e6b9c7cf09d777c15aa94c6a4ac2afc56d82ae80
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2015-09-30 09:58:24 -0600

    New features, better logic

--- LinkFlagger.js
+++ LinkFlagger.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LinkFlagger
-// @version     4
+// @version     10
 // @author      Grant Johnson
 // @description Highlights brainhoney and box links and images.
 // @include     *brightspace.com*
@@ -15,7 +15,6 @@ for (var i = 0; i < links.length; i++) {
     element = links[i];
     if (element.href.indexOf("https://byui.brainhoney") === 0) {
         element.style.color = "#ff0000";
-        element.style.backgroundColor = "#000000";
     }
 }
 
@@ -24,33 +23,48 @@ for (var i = 0; i < links.length; i++) {
     element = links[i];
     if (element.href.indexOf("https://app.box") === 0) {
         element.style.color = "#ff0000";
-        element.style.backgroundColor = "#000000";
     }
 }
 
 // Flags links that don't open in new window
 for (var i = 0; i < links.length; i++) {
     element = links[i];
-    if (element.target.indexOf("_blank") !== 0 && !element.hasAttribute("class") && element.href.indexOf("http") == 0) {
+    if (element.target.indexOf("_blank") !== 0 && !element.hasAttribute("class") && element.href.indexOf("javascript") !== 0 && !element.hasAttribute("name") && !element.hasAttribute("id"))  {
         element.style.border = "3px dotted #ffaa00";
     }
 }
 
+// Puts a red bar at the top if the title is wrong.
+var titles = document.getElementsByTagName('title');
+for (var i; i < links.length; i++) {
+    element = titles[i];
+    if (element.innerHTML == "BYU-Idaho") {
+        var body = document.getElementsByTagName('body');
+        var element1;
+        
+        var element1 = body[0];
+        
+        element.style.borderTop = "3px dotted #ff0000";
+    }
+
+
+}
+
 var linksimg = document.getElementsByTagName('img');
-var element;
+var images;
 
 // Flags images from brainhoney with red border
 for (var i = 0; i < linksimg.length; i++) {
-    element = linksimg[i];
-    if (element.src.indexOf("https://byui.brainhoney") === 0) {
-        element.style.border = "5px solid #ff0000";
+    images = linksimg[i];
+    if (images.src.indexOf("https://byui.brainhoney") === 0) {
+        images.style.border = "5px solid #ff0000";
     }
 }
 
 // Flags images without alt with a blue border. 
 for (var i = 0; i < linksimg.length; i++) {
-    element = linksimg[i];
-    if (!element.hasAttribute("alt")) {
-        element.style.outline = "5px solid #0000ff";
+    images = linksimg[i];
+    if (!images.hasAttribute("alt") && !images.hasAttribute("class") ) {
+        images.style.outline = "5px solid #0000ff";
     }
 }
