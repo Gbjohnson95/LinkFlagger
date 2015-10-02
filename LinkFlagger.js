802fa008dea4de76b32cce36ab773b1a5ce65930
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2015-10-02 09:58:32 -0600

    Now sets the title to the list of issues.

--- LinkFlagger.js
+++ LinkFlagger.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LinkFlagger
-// @version     13
+// @version     15
 // @author      Grant Johnson
 // @description Highlights brainhoney and box links and images.
 // @include     *brightspace.com*
@@ -13,27 +13,52 @@ var element;
 // Flags brainhoney links red
 for (var i = 0; i < links.length; i++) {
     element = links[i];
+    var newtitle = '';
+
     if (element.href.indexOf("https://byui.brainhoney") === 0) {
         element.style.color = "#ff0000";
-        element.setAttribute('title', 'This a iLearn 2.0 link and HAS TO be updated to iLearn 3.0. ');
+        if (element.title.indexOf('Issues') === 0) {
+            var curtitle = '';
+            curtitle = element.getAttribute('title');
+            newtitle = curtitle + 'This is an iLearn 2.0 link, ';
+        } else {
+            newtitle = 'Issues: This is an iLearn 2.0 link, ';
+        }
+        element.setAttribute('title', newtitle);
     }
 }
 
 // Flags box links red
 for (var i = 0; i < links.length; i++) {
     element = links[i];
+    var newtitle = '';
     if (element.href.indexOf("https://app.box") === 0) {
         element.style.color = "#ff0000";
-        element.setAttribute('title', 'This a Box link, Box has shut down functionality, Move to Equella. ');
+        if (element.title.indexOf('Issues') === 0) {
+            var curtitle = '';
+            curtitle = element.getAttribute('title');
+            newtitle = curtitle + 'This a Box link, ';
+        } else {
+            newtitle = 'Issues: This a Box link, ';
+        }
+        element.setAttribute('title', newtitle);
     }
 }
 
 // Flags links that don't open in new window
 for (var i = 0; i < links.length; i++) {
     element = links[i];
+    var newtitle = '';
     if (element.target.indexOf("_blank") !== 0 && !element.hasAttribute("class") && element.href.indexOf("javascript") !== 0 && !element.hasAttribute("name") && !element.hasAttribute("id") && !element.hasAttribute('role'))  {
         element.style.border = "3px dotted #ffaa00";
-        element.setAttribute('title', 'This link does not open in a new window, and needs to. ');
+        if (element.title.indexOf('Issues') === 0) {
+            var curtitle = '';
+            curtitle = element.getAttribute('title');
+            newtitle = curtitle + 'This link does not open in a new window, ';
+        } else {
+            newtitle = 'Issues: This link does not open in a new window, ';
+        }
+        element.setAttribute('title', newtitle);
     }
 }
 
@@ -43,17 +68,34 @@ var images;
 // Flags images from brainhoney with red border
 for (var i = 0; i < linksimg.length; i++) {
     images = linksimg[i];
+    var newtitle = '';
     if (images.src.indexOf("https://byui.brainhoney") === 0) {
         images.style.border = "5px solid #ff0000";
-        images.setAttribute('title', 'This Image is hosted by iLearn 2.0, and HAS TO be moved to iLearn 3.0. ');
+        if (images.title.indexOf('Issues') === 0) {
+            var curtitle = '';
+            curtitle = images.getAttribute('title');
+            newtitle = curtitle + 'This Image is hosted by iLearn 2.0, ';
+        } else {
+            newtitle = 'Issues: This Image is hosted by iLearn 2.0, ';
+        }
+        images.setAttribute('title', newtitle);
     }
 }
 
 // Flags images without alt with a blue border. 
 for (var i = 0; i < linksimg.length; i++) {
     images = linksimg[i];
+    var newtitle = '';
     if (!images.hasAttribute("alt") && !images.hasAttribute("class") ) {
         images.style.outline = "5px solid #0000ff";
+        if (images.title.indexOf('Issues') === 0) {
+            var curtitle = '';
+            curtitle = images.getAttribute('title');
+            newtitle = curtitle + 'This Image has no alt text, ';
+        } else {
+            newtitle = 'Issues: This Image has no alt text, ';
+        }
+        images.setAttribute('title', newtitle);
     }
 }
 
